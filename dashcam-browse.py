6749716378f23ecd4117b03c67c667e7ab9975a1
Vadim Zaliva <lord@crocodile.org>
2020-05-26 09:58:43 -0700

    proxying to JSON. debug mode

--- dashcam-browse.py
+++ dashcam-browse.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
 
-import configparser
-import argparse
-import xml.etree.ElementTree as ET
-
+import xmltodict
 import requests
-from bottle import route, run, template
-
-config = configparser.ConfigParser()
+import json
+from bottle import route, run, template, request, response
 
 # `ftype` must be "Normal" or "Event"
-def fetch_list(ftype, start=0, count=100,backward=0):
-    r = requests.get("{base}cgi-bin/Config.cgi?backward={backward}&count={count}&property={ftype}&from={xfrom}&action=dir&format=all".format(
-        backward=backward,
-        xfrom=start,
-        count=count,
-        base=config['CAMERA']['URL'],
-        ftype=ftype
-    ))
-    assert r.status_code == 200, "HTTP error %d" % r.status_code
-    assert r.headers["Content-Type"] == "application/xml"
-    response_body_as_xml = ET.fromstring(r.content)
-    xml_tree = ET.ElementTree(response_body_as_xml)
-    root = xml_tree.getroot()
-    assert root.tag == ftype, "unexpected root tag '%s'" % root.tag
-    return []
+# ?backward=1&count=30&property=Normal&from=0&action=dir&format=all
+@route('/list')
+def list():
+    if debug:
+        f = open("sample_list.xml", "r") 
+        xml = f.read()
+        f.close()
+    else:
+        r = requests.get("%scgi-bin/Config.cgi?%s" %
+                         (config['CAMERA']['URL'],request.query_string))
+        assert r.status_code == 200, "HTTP error %d" % r.status_code
+        assert r.headers["Content-Type"] == "application/xml"
+        xml = r.content
+    response.headers['Content-Type'] = 'application/json'
+    #response.set_header('Content-Type', 'application/json')
+    return json.dumps(xmltodict.parse(xml))
 
 @route('/')
 def index():
-    l = fetch_list("Normal")
     return '<b>Hello1</b>!'
 
 def main():
+    import argparse, configparser
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--config", default='dashcam.cfg', help="config file")
+    parser.add_argument("-d", '--debug', action='store_true')
     args = parser.parse_args()
+    global debug
+    debug = args.debug
+    global config
+    config = configparser.ConfigParser()
     config.read(args.config)
     run(host=config['WEB']['host'], port=config['WEB']['port'])
 
